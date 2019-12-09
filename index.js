456ad81146300bc92638419c1873786f31586dd2
Keegan DSouza (Collabera) <v-kedsou@microsoft.com>
2019-12-09 17:27:59 -0600

    adf

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 var port = process.env.port || 3000
 
 app.get('/', function (req, res) {
+  console.log("Request Made")
   res.send('Hello World!');
 });
 app.listen(port, function () {
